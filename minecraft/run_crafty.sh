37e38d96e28b069d1513e1aa68bc9bf9a62b5a30
cacaedumartins-alt <cacaedumartins@gmail.com>
2025-11-14 09:56:46 +0000

    Reset e estrutura inicial de backup para sincronização

--- minecraft/run_crafty.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /workspaces/minezinserver1/minecraft
-source .venv/bin/activate 
-cd crafty-4 
-exec python3 main.py 
--- minecraft/run_crafty_service.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /workspaces/minezinserver1/minecraft
-source .venv/bin/activate 
-cd crafty-4 
-python3 main.py -d
